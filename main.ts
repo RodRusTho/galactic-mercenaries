b1272783c504d8410d6e081fe5114f2a37553050
Rodney Thomas <114374607+RodRusTho@users.noreply.github.com>
2022-12-09 18:20:17 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,46 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . 
+        . . . . . . . . 
+        . . . . . . . . 
+        . . . . . . . . 
+        . . . 7 7 . . . 
+        . . . 7 7 . . . 
+        . . . 7 7 . . . 
+        . . . 7 7 . . . 
+        `, ship, 0, -140)
+    projectile.startEffect(effects.coolRadial, 100)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprite.destroy()
+    otherSprite.destroy(effects.disintegrate)
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    scene.cameraShake(4, 500)
+    otherSprite.destroy(effects.disintegrate)
+    sprite.startEffect(effects.fire, 200)
+    info.changeLifeBy(-1)
+})
+let projectile: Sprite = null
+let ship: Sprite = null
+game.showLongText("Galactic Mercenaries - Infrared Productions", DialogLayout.Full)
+let asteroids = [
+sprites.projectile.star5,
+sprites.projectile.flash2,
+sprites.projectile.flash1,
+sprites.projectile.star3,
+sprites.projectile.star2,
+sprites.projectile.star1
+]
+ship = sprites.create(sprites.space.spaceGreenShip, SpriteKind.Player)
+ship.setStayInScreen(true)
+ship.bottom = 120
+controller.moveSprite(ship, 100, 100)
+info.setLife(3)
+effects.confetti.startScreenEffect()
+game.onUpdateInterval(500, function () {
+    projectile = sprites.createProjectileFromSide(asteroids[randint(0, asteroids.length - 1)], 0, 75)
+    projectile.setKind(SpriteKind.Enemy)
+    projectile.x = randint(10, 150)
+})
